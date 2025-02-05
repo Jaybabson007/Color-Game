8ec587c4f113f4e194003a27bcdecdf1326b75c0
Jaybabson007 <yahayajoel2@gmail.com>
2025-02-05 15:39:44 +0100

    Initialised color palettes

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let score = 0
     let correctColor = ""
   
-  // DOM elements
-  const colorBox = document.querySelector('[data-testid="colorBox"]')
-  const colorOptions = document.querySelectorAll('[data-testid="colorOption"]')
-  const gameStatus = document.querySelector('[data-testid="gameStatus"]')
-  const scoreElement = document.querySelector('[data-testid="score"]')
-  const newGameButton = document.querySelector('[data-testid="newGameButton"]')
+    // DOM elements
+    const colorBox = document.querySelector('[data-testid="colorBox"]')
+    const colorOptions = document.querySelectorAll('[data-testid="colorOption"]')
+    const gameStatus = document.querySelector('[data-testid="gameStatus"]')
+    const scoreElement = document.querySelector('[data-testid="score"]')
+    const newGameButton = document.querySelector('[data-testid="newGameButton"]')
+
+    
