e98435d5def5d2e04a49f8beffe0f2be5fc8e24f
Jaybabson007 <yahayajoel2@gmail.com>
2025-02-05 15:37:24 +0100

    Initialised DOM elements

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
     let score = 0
     let correctColor = ""
   
+  // DOM elements
+  const colorBox = document.querySelector('[data-testid="colorBox"]')
+  const colorOptions = document.querySelectorAll('[data-testid="colorOption"]')
+  const gameStatus = document.querySelector('[data-testid="gameStatus"]')
+  const scoreElement = document.querySelector('[data-testid="score"]')
+  const newGameButton = document.querySelector('[data-testid="newGameButton"]')
